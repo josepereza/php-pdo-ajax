1210e50d03c7bffe918507596aa8c26a8d8e2f74
josepereza <48152618+josepereza@users.noreply.github.com>
2019-04-03 11:17:07 +0200

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,7 +1,7 @@
 <?php
 
 $username  = 'root';
-$password  = '3266root';
+$password  = '123456';
 $result = 0;
 try {
     $dbconn = new PDO('mysql:host=localhost;dbname=onlinestore', $username, $password);
